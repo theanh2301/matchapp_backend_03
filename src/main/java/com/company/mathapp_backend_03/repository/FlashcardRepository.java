0e966192f0f46da83eed25f4cd29fcdcffa58a77
theanh <becam0582@gmail.com>
2026-03-20 14:29:20 +0700

    add code flashcard

--- src/main/java/com/company/mathapp_backend_03/repository/FlashcardRepository.java
+++ src/main/java/com/company/mathapp_backend_03/repository/FlashcardRepository.java
@@ -1,10 +1,20 @@
 package com.company.mathapp_backend_03.repository;
 
 import com.company.mathapp_backend_03.entity.Flashcard;
+import com.company.mathapp_backend_03.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FlashcardRepository extends JpaRepository<Flashcard, Integer> {
     boolean existsByLessonId(Integer id);
+
+    List<Flashcard> findByLessonId(Integer id);
+
+    Optional<Flashcard> findByFrontTextAndBackTextAndLesson(String frontText, String backText, Lesson lesson);
+
+    boolean existsByFrontTextAndBackTextAndLessonAndIdNot(String trim, String trim1, Lesson lesson, Integer id);
 }
---
+++ src/main/java/com/company/mathapp_backend_03/rest/FlashcardApi.java
@@ -0,0 +1,46 @@
+package com.company.mathapp_backend_03.rest;
+
+import com.company.mathapp_backend_03.model.request.FlashcardRequest;
+import com.company.mathapp_backend_03.model.response.FlashcardResponse;
+import com.company.mathapp_backend_03.service.FlashcardService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/flashcards")
+@RequiredArgsConstructor
+public class FlashcardApi {
+
+    private final FlashcardService flashcardService;
+
+    @GetMapping("/{lessonId}")
+    public List<FlashcardResponse> getFlashcards(@PathVariable Integer lessonId) {
+        return flashcardService.getFlashcard(lessonId);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addFlashcard (@Valid @RequestBody FlashcardRequest flashcardRequest) {
+        flashcardService.addFlashcard(flashcardRequest);
+        return ResponseEntity.ok("Flashcard created successfully");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateFlashcard(@PathVariable Integer id,
+                                             @RequestBody FlashcardRequest flashcardRequest) {
+        flashcardService.updateFlashcard(id, flashcardRequest);
+
+        return ResponseEntity.ok("Flashcard updated successfully");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteFlashcard(@PathVariable Integer id) {
+        flashcardService.deleteFlashcard(id);
+
+        return ResponseEntity.ok("Flashcard deleted successfully");
+    }
+
+}
